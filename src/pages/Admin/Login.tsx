84119d46505ee332642cb7c7234ea13d081116df
Kevin Roan <kevinroan@proton.me>
2024-01-30 00:52:57 +0530

    fix: admin navigation

--- src/pages/Admin/Login.tsx
+++ src/pages/Admin/Login.tsx
@@ -1,19 +1,18 @@
-import { signIn } from '../../Helpers/firebaseAuth.js'
-import { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { signIn } from "../../Helpers/firebaseAuth.js";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface FirebaseUser {
-  uid:string;
-  email:string;
+  uid: string;
+  email: string;
 }
-
 
 function Login() {
   const navigate = useNavigate();
-  const [formData,setFormData] = useState({
-    email:'',
-    password:'',
-  })
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -22,33 +21,53 @@ function Login() {
       [name]: value,
     }));
   };
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
-   e.preventDefault(); 
-  }
-const handleSignIn = () => {
-  signIn(formData.email, formData.password)
-    .then((user:FirebaseUser | undefined) => {
-      console.log("User signed in:", user);
-      navigate('/admin/dashboard');
-    })
-    .catch((error: { code: string; message: string }) => {
-      console.error("Sign-in failed:", error);
-    });
-};
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  const handleSignIn = () => {
+    signIn(formData.email, formData.password)
+      .then((user: FirebaseUser | undefined) => {
+        console.log("User signed in:", user);
+        navigate("/admin/studentlist");
+      })
+      .catch((error: { code: string; message: string }) => {
+        console.error("Sign-in failed:", error);
+      });
+  };
   return (
     <>
-    <form onClick={handleSubmit}>
-    <div className="login_container">
-      <div className="login_form_wrapper">
-        <label>Admin Login</label>
-      <input placeholder="Enter Email Address" type="email" name="email" pattern="[^\s@]+@[^\s@]+\.[^\s@]+" required value={formData.email} onChange={handleChange}/>
-      <input placeholder="Enter Password" type="password" name="password" required value={formData.password} onChange={handleChange}/>
-      <input type="submit" className="button_light" value="submit" onClick={handleSignIn}/>
-      </div>
-    </div>
-</form>
-</>
-  )
+      <form onClick={handleSubmit}>
+        <div className="login_container">
+          <div className="login_form_wrapper">
+            <label>Admin Login</label>
+            <input
+              placeholder="Enter Email Address"
+              type="email"
+              name="email"
+              pattern="[^\s@]+@[^\s@]+\.[^\s@]+"
+              required
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <input
+              placeholder="Enter Password"
+              type="password"
+              name="password"
+              required
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <input
+              type="submit"
+              className="button_light"
+              value="submit"
+              onClick={handleSignIn}
+            />
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
 
-export default Login
+export default Login;
